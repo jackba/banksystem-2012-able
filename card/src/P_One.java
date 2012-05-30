89657dc1deb5dd79cd07c53c5ce082b08145903d
sunny3371@gmail.com <sunny3371@gmail.com@574d59a2-f3ce-4054-e5af-a15519ee7797>
2012-05-30 11:00:26 +0000

    변수 이름 변경

--- card/src/P_One.java
+++ card/src/P_One.java
@@ -7,15 +7,14 @@ public class P_One extends JPanel {
     private Frame F;
      
     public P_One(Frame f) {
-    	JPasswordField jPasswordField1;
-    	final JTextField jTextField8;
+    	JPasswordField pwField;
+    	final JTextField idField;
     	JLabel jLabel19;
     	JLabel jLabel20;
     	final JButton jButton1;
 
     	    	
         setBackground(Color.LIGHT_GRAY);
-        //setTitle("Login");
         setBounds(94, 20, 854, 551);
 
         setLayout(null);
@@ -23,21 +22,19 @@ public class P_One extends JPanel {
         F = f;
          
        
-        jPasswordField1 = new JPasswordField();
-		//jPasswordField1.setText("jPasswordField1");
-		jPasswordField1.setFont(new Font("Dialog.plain", 0, 12));
-		jPasswordField1.setForeground(new Color(-13421773));
-		jPasswordField1.setBackground(new Color(-1));
-		jPasswordField1.setBounds(188, 98, 139, 46);
-		add(jPasswordField1);
+        pwField = new JPasswordField();
+		pwField.setFont(new Font("Dialog.plain", 0, 12));
+		pwField.setForeground(new Color(-13421773));
+		pwField.setBackground(new Color(-1));
+		pwField.setBounds(188, 98, 139, 46);
+		add(pwField);
 
-		jTextField8 = new JTextField();
-		//jTextField8.setText("jTextField8");
-		jTextField8.setFont(new Font("Dialog.plain", 0, 12));
-		jTextField8.setForeground(new Color(-13421773));
-		jTextField8.setBackground(new Color(-1));
-		jTextField8.setBounds(188, 50, 140, 42);
-		add(jTextField8);
+		idField = new JTextField();
+		idField.setFont(new Font("Dialog.plain", 0, 12));
+		idField.setForeground(new Color(-13421773));
+		idField.setBackground(new Color(-1));
+		idField.setBounds(188, 50, 140, 42);
+		add(idField);
 
 		jLabel19 = new JLabel();
 		jLabel19.setText("아이디");
@@ -57,7 +54,7 @@ public class P_One extends JPanel {
 		jLabel20.setBounds(81, 108, 69, 24);
 		add(jLabel20);
 
-		 jButton1 = new JButton();
+		jButton1 = new JButton();
 		jButton1.setText("관리자 로그인");
 		jButton1.setFont(new Font("Dialog.plain", 0, 12));
 		jButton1.setIcon(new ImageIcon(""));
@@ -73,8 +70,7 @@ public class P_One extends JPanel {
 		
 		jButton1.addActionListener(new ActionListener() {
 	            public void actionPerformed(ActionEvent arg0) {
-	            	
-	                   	F.changePanel();
+	            	F.changePanel();
 	            }
 	    });
 		
